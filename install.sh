2d2f653ecbc22423e754976a2c66350e84c51cbe
thanh2009 <tempmail2k9@gmail.com>
2025-12-15 22:00:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg install python-pip && pkg install python tsu libexpat openssl -y && pip install requests Flask colorama aiohttp psutil
-curl -Ls "https://raw.githubusercontent.com/thanh-26009/hack-roblox/refs/heads/main/rejoin.py" -o /sdcard/Download/rejoin.py
+curl -Ls "https://raw.githubusercontent.com/thanh-26009/hack-roblox/refs/heads/main/rejoin.py" -o rejoin.py
+python3 rejoin.py
 
 
